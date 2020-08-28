cc81fc5e1294b6b1fbe90f79873dd4ddb5b61b86
zarraga555 <zarraga555@gmail.com>
2020-08-28 15:59:45 -0400

    modificacion en app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
+import { MenuComponent } from './menu/menu.component';
+import { BannerComponent } from './banner/banner.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { Error404Component } from './error404/error404.component';
 
+const routes: Routes = [
+  {path: '', component: HomeComponent },
+  {path: 'login', component: LoginComponent },
+  {path: '**', component: Error404Component },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HolaMundoComponent,
+    MenuComponent,
+    BannerComponent,
+    FooterComponent,
+    LoginComponent,
+    HomeComponent,
+    Error404Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
